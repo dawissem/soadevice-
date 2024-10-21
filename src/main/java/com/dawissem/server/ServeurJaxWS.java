de3d2b0dd52b65c50a097b1f3fba7eddafd21d06
dawissem <dawissem.wm@gmail.com>
2024-10-21 11:31:21 +0100

    sencd commit

--- src/main/java/com/dawissem/server/ServeurJaxWS.java
+++ src/main/java/com/dawissem/server/ServeurJaxWS.java
@@ -3,12 +3,12 @@ package com.dawissem.server;
 import com.dawissem.metier.BanqueService;
 import jakarta.xml.ws.Endpoint;
 public class ServeurJaxWS {
-    public class Main {
+
         public static void main(String[] args) {
 
             String url = "http://0.0.0.0:8686/";
             Endpoint.publish(url, new BanqueService());
             System.out.println("web Service deploayer sure " + url);
         }
-    }
+
 }
